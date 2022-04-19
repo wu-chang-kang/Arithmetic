2f4616dba724e810cd4b5e4f0eea12ba456eff9c
wu-chang-kang <1147552381@qq.com>
2022-04-19 23:12:38 +0800

    add

--- 初级算法/number/16-统计各位数字都不同的数字个数.ts
+++ 初级算法/number/16-统计各位数字都不同的数字个数.ts
@@ -2,6 +2,6 @@ const countNumbersWithUniqueDigts = (n: number): number => {
     const ans = [];
     for(let i = 0; i < 10 ** n; i++) {
         ans.push(i);
-    }
+    } 
     return ans.filter(num => [...num.toString()].length === new Set([...num.toString()]).size).length;
 }
