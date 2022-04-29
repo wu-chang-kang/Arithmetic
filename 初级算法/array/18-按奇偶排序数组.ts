4b3568ab21b81478e57040d07d8b3863fe53a45b
wu-chang-kang <1147552381@qq.com>
2022-04-29 11:58:29 +0800

    fix

---
+++ 初级算法/array/18-按奇偶排序数组.ts
@@ -0,0 +1,3 @@
+export const sortArrayByParity = (nums: number[]): number[] => {
+    return nums.sort(a => (a & 1 ? 1 : -1));
+  };
--- 初级算法/number/18-按奇偶排序数组.ts
+++
@@ -1,7 +0,0 @@
-const sortArrayByParity = (nums: number[]): number[]  => {
-    const [a, b] = [[], []];
-    nums.forEach(n => {
-        ((n & 1) === 0 ? a : b).push(n);
-    });
-    return [...a, ...b];
-};
