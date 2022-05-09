189e22b6184f04a1e29277881a724bf017e5781b
wu-chang-kang <1147552381@qq.com>
2022-05-09 08:41:59 +0800

    feat: 新增每日一题

--- 每日一题/22-05/442. 数组中重复的数据.ts
+++ 每日一题/22-05/442. 数组中重复的数据.ts
@@ -1,10 +1,20 @@
 export const findDuplicates = (nums: number[]): number[] => {
-    const set = new Set<number>();
-    const ans: number[] = [];
-    nums.forEach(item => {
-      if (set.size === set.add(item).size) {
-        ans.push(item);
-      }
-    });
-    return ans;
+  const swap = (nums: number[], index1: number, index2: number) => {
+    const temp = nums[index1]
+    nums[index1] = nums[index2]
+    nums[index2] = temp
+  }
+  const n = nums.length
+  for (let i = 0; i < n; ++i) {
+    while (nums[i] != nums[nums[i] - 1]) {
+      swap(nums, i, nums[i] - 1)
+    }
+  }
+  const ans: number[] = []
+  for (let i = 0; i < n; ++i) {
+    if (nums[i] - 1 !== i) {
+      ans.push(nums[i])
+    }
+  }
+  return ans
 };
