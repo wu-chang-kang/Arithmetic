bb34004024125dadf8485b09020b0a09fd50faa8
wu-chang-kang <1147552381@qq.com>
2022-07-17 22:50:20 +0800

    feat: 新增每日一题

---
+++ 每日一题/22-07/565. 数组嵌套.ts
@@ -0,0 +1,14 @@
+export function arrayNesting(nums: number[]): number {
+    let ans = 0, n = nums.length;
+    const vis = new Array(n).fill(0);
+    for (let i = 0; i < n; ++i) {
+        let cnt = 0;
+        while (!vis[i]) {
+            vis[i] = true;
+            i = nums[i];
+            ++cnt;
+        }
+        ans = Math.max(ans, cnt);
+    }
+    return ans;
+};
